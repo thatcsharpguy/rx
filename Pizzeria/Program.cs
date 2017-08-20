1f8c404c85417409f7fe6803f22fbdf0952a95b7
Antonio Feregrino <antonio.feregrino@gmail.com>
2017-08-20 10:40:32 -0500

    Pizzeria reactiva

---
+++ Pizzeria/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pizzeria
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
